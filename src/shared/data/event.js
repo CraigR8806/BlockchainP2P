cd48a267b919f4c8bc2831af281242b7408c2e2e
Craig Ramage <craig@craigramage.com>
2023-10-02 21:39:02 -0400

    removed remaining js files

--- src/shared/data/event.js
+++
@@ -1,7 +0,0 @@
-module.exports = class Event {
-    constructor(eventType, eventInfo, requestor) {
-        this.eventType = eventType;
-        this.eventInfo = eventInfo;
-        this.requestor = requestor;
-    }
-}
